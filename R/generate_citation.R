6f8e5ee8444105d4feb804d3bf53350690b093d4
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-03-13 17:27:11 -0400

    Create function to generate report citation

---
+++ R/generate_citation.R
@@ -0,0 +1,12 @@
+generate_citation <- function(
+    authors = NULL,
+    title = NULL,
+    year = NULL
+    ){
+
+  # Create citation string
+  cit <- paste0("")
+
+  # Add citation as .qmd to add into template
+
+}
