68b9d99eade95fe1d99d1d54727f1866ccab88e5
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-03-20 17:07:59 -0400

    testing

---
+++ test/test_template_functionality.R
@@ -0,0 +1,76 @@
+# Testing variations of templates
+
+# Last update and testing 3/20/24
+
+### PLEASE DO NOT PUSH ANY TEMPLATES THAT YOU MAKE TESTING OUT THIS FUNCTION###
+# More guidance for saving scripts will be provided in the future
+
+# reference functions during testing
+source(here::here('R', 'create_template.R'))
+source(here::here('R', 'write_title.R'))
+source(here::here('R', 'paste_child.R'))
+source(here::here('R', 'chunkr.R'))
+source(here::here('R', 'generate_citation.R'))
+
+# Basic parameters
+# new_template = FALSE
+# prev_year = 2021
+# year = 2024
+# office = "SEFSC"
+# region = "Gulf of Mexico"
+# species = "Red Snapper"
+# type = "RT"
+
+################################################################################
+
+# Basic, new default template
+# Last tested 3/20/24
+create_template(
+  new_template = TRUE,
+  format = "pdf",
+  office = "NEFSC",
+  region = "GB",
+  species = "Bluefish",
+  spp_latin = "bluishfihesi",
+  year = 2024,
+  author = c("John Snow", "Danny Phantom", "Patrick Star"),
+  include_affiliation = TRUE,
+  parameters = TRUE,
+  param_names = c("fleet1", "fleet2", "model"),
+  param_values = c("Commercial", "Recreational", "Woods Hole Assessment Model"),
+  type = "RT"
+)
+
+################################################################################
+
+# Test pulling template from older assessment
+# Last tested 3/20/24
+# "Old" assessment template
+create_template(
+  new_template = TRUE,
+  format = "pdf",
+  office = "SEFSC",
+  region = "Gulf of Mexico",
+  species = "Red Snapper",
+  spp_latin = "Lutjanus campechanus",
+  year = 2021,
+  author = c("John Snow", "Danny Phantom", "Patrick Star"),
+  include_affiliation = TRUE,
+  parameters = FALSE,
+  type = "RT"
+)
+# New assessment from old template
+create_template(
+  new_template = FALSE,
+  prev_year = 2021,
+  year = 2024,
+  office = "SEFSC",
+  region = "Gulf of Mexico",
+  species = "Red Snapper",
+  latin = "Lutjanus campechanus",
+  type = "RT"
+)
+
+################################################################################
+
+
--- test/testing_functions.R
+++ test/testing_functions.R
@@ -2,7 +2,7 @@
 
 # Example script that allows the user to try out creating stock assessment report templates
 # In development
-# Last edited: 03/14/24
+# Last edited: 03/20/24
 
 ### PLEASE DO NOT PUSH ANY TEMPLATES THAT YOU MAKE TESTING OUT THIS FUNCTION###
 # More guidance for saving scripts will be provided in the future
@@ -12,85 +12,8 @@ source(here::here('R', 'create_template.R'))
 source(here::here('R', 'write_title.R'))
 source(here::here('R', 'paste_child.R'))
 source(here::here('R', 'chunkr.R'))
+source(here::here('R', 'generate_citation.R'))
 
-# test generating new template
-create_template(
-    new_template = TRUE,
-    format = "pdf",
-    office = "NEFSC",
-    region = "GB",
-    species = "Bluefish",
-    year = 2024,
-    author = c("John Snow", "Danny Phantom", "Patrick Star"),
-    include_affiliation = TRUE,
-    parameters = TRUE,
-    param_names = c("fleet1", "fleet2", "model"),
-    param_values = c("Commercial", "Recreational", "Woods Hole Assessment Model"),
-    type = "RT"
-)
-
-
-# new_template = TRUE
-# format = "pdf"
-# office = "NEFSC"
-# region = "GB"
-# species = "Bluefish"
-# year = 2024
-# author = c("John Snow", "Danny Phantom", "Patrick Star")
-# parameters = TRUE
-# param_names = c("office","region","species")
-# param_values = c("NEFSC","Georges Bank", "Bluefish")
-# add_section = FALSE
-# type = "RT"
-
-# Author affiliation reference
-  # cat(paste("format: \n",
-  #           "  " , "pdf", ": \n",
-  #           "  ", "  ", "keep-tex: ", "true \n",
-  #           "  " , "  ", "template-partials: \n",
-  #           "  ", "  ", "  ", " - title.tex \n",
-  #           "  ", "  ", "  ", " - sadraft.tex \n",
-  #           "  ", "  ", "include-in-header: \n",
-  #           "  ", "  ", "  ", "text: | \n",
-  #           "  ", "  ", "  ", "  ", "\\usepackage[noblocks]{authblk} \n",
-  #           "  ", "  ", "  ", "  ", "\\renewcommand*{\\Authsep}{, } \n",
-  #           "  ", "  ", "  ", "  ", "\\renewcommand*{\\Authand}{, } \n",
-  #           "  ", "  ", "  ", "  ", "\\renewcommand*{\\Authands}{, } \n",
-  #           "  ", "  ", "  ", "  ", "\\renewcommand\\Affilfont{\\small} \n",
-  #           sep = ""))
-
-# Test template pull from previous assessment
-# build "old template"
-create_template(
-  new_template = TRUE,
-  format = "pdf",
-  office = "SEFSC",
-  region = "Gulf of Mexico",
-  species = "Red Snapper",
-  year = 2021,
-  author = c("John Snow", "Danny Phantom", "Patrick Star"),
-  include_affiliation = TRUE,
-  parameters = FALSE,
-  type = "RT"
-)
-
-# Pull new one
-create_template(new_template = FALSE,
-                prev_year = 2021,
-                year = 2024,
-                office = "SEFSC",
-                region = "Gulf of Mexico",
-                species = "Red Snapper",
-                type = "RT"
-                )
-
-# new_template = FALSE
-# prev_year = 2021
-# year = 2024
-# office = "SEFSC"
-# region = "Gulf of Mexico"
-# species = "Red Snapper"
-# type = "RT"
 
 title1 <- write_title(
   office = "NEFSC",
@@ -105,3 +28,13 @@ cat(generate_citation(
   year = 2021,
   office = "NEFSC"
 ))
+
+# test paste_child
+cat(
+  paste_child(
+    c("00_abstract.qmd", "01_executive_summary.qmd", "11_appendix.qmd"),
+    label = c("abstract", "executive summary","appendix")
+  )
+)
+
+x=c("00_abstract.qmd", "01_executive_summary.qmd", "11_appendix.qmd")
