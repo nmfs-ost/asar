7a6268a27784916cdf3160f27a8e108e779d32c8
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-03-25 10:11:02 -0400

    Add css style function and gitignore

---
+++ R/create_style_css.R
@@ -0,0 +1,20 @@
+create_styles_css <- function(
+    species = NULL,
+    savedir = NULL
+){
+  #Add species image to title
+  spp_name <- sub(" ", "_", species)
+  spp_image <- list.files(path = here::here('inst', 'resources', 'spp_img'), pattern = spp_name)
+  styles_css <- paste(
+    ".quarto-title-block .quarto-title-banner {", "\n",
+      "  ", "background-image: ", spp_image, ";", "\n",
+      "  ", "background-size: 300px;", "\n",
+      "  ", "background-position: left;", "\n",
+      "  ", "background-repeat: no-repeat;", "\n",
+      "  ", "padding-left: 10px;", "\n",
+      "  ", "background-origin: content-box;", "\n",
+    "}", "\n"
+  )
+
+  utils::capture.output(cat(styles_css), file = paste0(savedir, "/", "styles.css"), append = FALSE)
+}
