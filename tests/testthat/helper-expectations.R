bc60490294e6f421ee7cf8a46b9c73c600b3f841
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-04-01 11:38:31 -0400

    Add testing and helper expectations

---
+++ tests/testthat/helper-expectations.R
@@ -0,0 +1,9 @@
+# Helper expectations functions
+
+expect_output <- function() {
+
+}
+
+expect_save <- function() {
+
+}
---
+++ tests/testthat/test-create_template.R
@@ -0,0 +1,3 @@
+test_that("creates template .qmd file", {
+  expect_equal(2 * 2, 4)
+})
