db4be2964ef6e76254959ddc0d0294db9334eba4
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-04-05 16:21:35 -0400

    template testing (informal)

--- test/test_template_functionality.R
+++ test/test_template_functionality.R
@@ -13,7 +13,39 @@ source(here::here('R', 'chunkr.R'))
 source(here::here('R', 'generate_citation.R'))
 source(here::here('R', 'create_style_css.R'))
 
+# Option and parameters for tempalte generation
+new_template = TRUE
+tempdir = here::here()
+format = c("pdf", "docx", "html", NULL)
+office = c("AFSC", "PIFSC", "NEFSC", "NWFSC", "SEFSC", "SWFSC")
+region = NULL
+complex = "NO"
+species = NULL
+spp_latin = NULL
+year = NULL
+author = NULL
+include_affiliation = FALSE
+simple_affiliation = TRUE
+alt_title = FALSE
+title = NULL
+parameters = TRUE
+param_names = NULL
+param_values = NULL
+resdir = NULL
+model_results = NULL
+model = NULL
+add_section = FALSE
+secdir = NULL
+new_section = NULL
+section_location = NULL
+type = NULL
+prev_year = NULL
+custom = FALSE
+custom_sections = NULL
+
+################################################################################
 # Basic parameters
+# Last tested 04-05-24
 new_template = TRUE
 format = "pdf"
 office = "NEFSC"
@@ -27,11 +59,13 @@ parameters = TRUE
 param_names = c("fleet1", "fleet2", "model")
 param_values = c("Commercial", "Recreational", "Woods Hole Assessment Model")
 type = "RT"
+model_results = "report.sso"
+model = "SS"
 
 ################################################################################
 
 # Basic, new default template
-# Last tested 3/20/24
+# Last tested0 3-20-24
 create_template(
   new_template = TRUE,
   format = "pdf",
@@ -53,8 +87,9 @@ create_template(
 ################################################################################
 
 # Test pulling template from older assessment
-# Last tested 3/20/24
+# Last tested 3-20-24
 # "Old" assessment template
+
 create_template(
   new_template = TRUE,
   format = "pdf",
@@ -69,6 +104,7 @@ create_template(
   type = "RT"
 )
 # New assessment from old template
+
 create_template(
   new_template = FALSE,
   prev_year = 2021,
@@ -82,4 +118,29 @@ create_template(
 
 ################################################################################
 
+# Create assessment with custom sections using those in the repo already
+# Last tested 04-05-24
 
+new_template = TRUE
+format = "pdf"
+office = "NEFSC"
+region = "GB"
+species = "Bluefish"
+spp_latin = "bluishfihesi"
+year = 2024
+author = c("John Snow", "Danny Phantom", "Patrick Star")
+include_affiliation = TRUE
+parameters = TRUE
+param_names = c("fleet1", "fleet2", "model")
+param_values = c("Commercial", "Recreational", "Woods Hole Assessment Model")
+type = "RT"
+custom = TRUE
+add_section = FALSE
+custom_sections = c("executive summary", "data", "model", "results", "references", "tables", "figures")
+tempdir = here::here()
+complex = "NO"
+simple_affiliation = TRUE
+alt_title = FALSE
+resdir = "C:/Users/samantha.schiano/Documents/Stock Assessment Workflow/ex_mod_res"
+model_results = "output.Rdata"
+model = "WHAM"
