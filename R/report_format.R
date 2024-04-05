2187c90fbcb76509732c27031a79c22d2e9da4ba
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-04-05 11:00:51 -0400

    Add documentation for formatting report (styling)

--- R/report_format.R
+++ R/report_format.R
@@ -1,9 +1,13 @@
+#' Add FSC Formatting
+#'
+#' @param office office the analyst is associated with and in turn will active the formatting for the RFMO in the region
+#'
+#' @return Change the formatting of the report based on current requirements from the fishery management councils to the fishery science centers.
+#' @export
+#'
+#' @examples report_format(office = "NEFSC")
 report_format <- function(
-    type = NULL,
     office = NULL) {
-  # This may need to be changed bc not every region has different format - maybe just different format based on report type (OA vs RT)
-  # Add report formatting based on report type (operational/research)
-  if (type == "OA" | type == "UP" | type == "MT") {
     # Add report formatting based on region
     if (office == "AFSC") {
 
@@ -23,24 +27,4 @@ report_format <- function(
     } else {
       print("office (FSC) is not defined. Please define which office you are associated with.")
     }
-  } else if (type == "RT" | type == "FULL") {
-    # Add report formatting based on region
-    if (office == "AFSC") {
-
-    } else if (office == "NEFSC") {
-
-    } else if (office == "NWFSC") {
-
-    } else if (office == "PIFSC") {
-
-    } else if (office == "SEFSC") {
-
-    } else if (office == "SWFSC") {
-
-    } else {
-      print("office (FSC) is not defined. Please define which office you are associated with.")
-    }
-  } else {
-    print("Type of assessment report is not defined")
-  }
 }
