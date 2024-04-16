e894e982f8e818c418d0a804cd11043ee66e3cf4
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-04-16 14:53:48 -0400

    Update add_section fxn to use code from template

--- R/add_section.R
+++ R/add_section.R
@@ -1,9 +1,9 @@
 #' Add New Section to Template
 #'
-#' @param title title of the subsection. Please keep this short as the template
-#'              will be created as a quarto document into your skeleton and saved
-#'              later for reference. More than one section can be added. Please
-#'              make a list if this is desired.
+#' @param sec_names section_names of the subsection. Please keep this short as the template
+#'                  will be created as a quarto document into your skeleton and saved
+#'                  later for reference. More than one section can be added. Please
+#'                  make a list if this is desired.
 #' @param location Where the section will be added in the generic template using
 #'                 the notation of 'placement-section'. Example would be 'in-introduction'
 #'                 where the new content would be created as a child document
@@ -11,7 +11,9 @@
 #'                 add more than one section. Add the location as a list
 #'                 corresponding to the order of section names listed in the
 #'                 'title' parameter
-#' @param multi TRUE/FALSE, more than one section is being added
+#' @param other_sections List of other sections that the added sections will be combined with (if not NULL)
+#' @param subdir directory where the new sections will be saved - in the create_template
+#'               fxn this defaults to the location where the template is saved
 #'
 #' @return Add an additional section to the report template if it is not already
 #'         in the default template. This provides the option to adding it as a
@@ -19,26 +21,45 @@
 #'         as a child document.
 #' @export
 #'
-#' @examples add_section(title = "Ecosystem Considerations", location = "after-discussion")
+#' @examples add_section(sec_names = "Ecosystem Considerations", location = "after-discussion",
+#' other_sections = c("introduction.qmd", "model.qmd", "results.qmd", "discussion.qmd"),
+#' subdir = tempdir())
 add_section <- function(
-    title = NULL,
+    sec_names = NULL,
     location = NULL,
-    multi = FALSE) {
+    other_sections = NULL,
+    subdir = NULL) {
   # Location options
   # before-section
   # after-section
-  # in-section (will always apend to the end of the section)
-  section_name <- forstringr::str_extract_part(location, "-", before = FALSE)
+  # in-section (will always append to the end of the section)
+  for(i in 1:length(sec_names)){
+    section_i_name <- paste0(sec_names[i], ".qmd")
+    local_section <- forstringr::str_extract_part(location[i], "-", before = FALSE)
 
-  if (grepl("before-", location)) {
+    if(any("TRUE" %in% grepl("-", location))){
+      locality <- forstringr::str_extract_part(location[i], "-", before = TRUE)
+    } else if(any("TRUE" %in% grepl(" ", location))){
+      locality <- forstringr::str_extract_part(location[i], " ", before = TRUE)
+    }
 
+    section_i <- paste0("## ", stringr::str_to_title(sub("_", " ", sec_names[i])), "\n",
+                        "\n",
+                        "[Insert text here]", "\n",
+                        "\n",
+                        chunkr("# Insert code", label = "example_chunk"), "\n"
+    )
+    utils::capture.output(cat(section_i), file = paste0(subdir, "/", section_i_name), append = FALSE)
+
+    if(locality=="before"){
+      other_sections <- append(other_sections, section_i_name, after = (which(grepl(local_section, other_sections))-1))
+    } else if (locality=="after"){
+      other_sections <- append(other_sections, section_i_name, after = which(grepl(local_section, other_sections)))
+    } else if (locality=="in"){
+      stop("No available option for adding a new section 'in' another quarto document.", call. = FALSE)
+    } else {
+      stop("Invalid selection for placement of section. Please name the follow the format 'placement-section_name' for adding a new section.")
+    }
   }
-
-  if (grepl("in-", location)) {
-
-  }
-
-  if (grepl("after-", location)) {
-
-  }
+  return(other_sections)
 }
