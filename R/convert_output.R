9df390faec9527333f7a1696458da429c7418aa5
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-07-26 10:40:27 -0400

    Set up dataframe for format and add read for SS3 reportfiel

--- R/convert_output.R
+++ R/convert_output.R
@@ -20,27 +20,77 @@ convert_output <- function(
     output.file = NULL,
     outdir = NULL,
     model = NULL) {
+  # Blank dataframe and set up to mold output into
+  out_new <- data.frame(label = NA,
+                        time = NA,
+                        fleet = NA,
+                        area = NA,
+                        season = NA,
+                        age = NA,
+                        sex = NA,
+                        intial = NA,
+                        estimate = NA,
+                        uncertainty = NA,
+                        likelihood = NA,
+                        gradient = NA,
+                        estimated = NA # TRUE/FALSE
+                        )
+
   # Convert SS3 output Report.sso file
   if (model == "ss") {
+    # read SS report file
+    # Associated function to extract columns for table - from r4ss
+    get_ncol <- function(file, skip = 0) {
+      nummax <- max(utils::count.fields(file,
+                                        skip = skip, quote = "",
+                                        comment.char = ""
+      )) + 1
+      return(nummax)
+    }
+    # Read as table
+    dat <- read.table(
+      file = output.file,
+      col.names = 1:get_ncol(output.file),
+      fill = TRUE,
+      quote = "",
+      colClasses = "character", # reads all data as characters
+      nrows = -1,
+      comment.char = "",
+      blank.lines.skip = FALSE
+    )
+
     # Step 1 identify and extract breaks in output
-    # Function to extract rows where values are present for identifies labels
-    # extract example param
-    next_blank_row <- function(output, label) {
+    # Function to extract rows, identify the dfs, and clean them up
+    SS3_extract_df <- function(dat, label){
       # Locate the row containing the specified value from the df
-      value_row <- which(apply(output, 1, function(row) any(row == label)))[2]
+      value_row <- which(apply(dat, 1, function(row) any(row == label)))[2]
 
       # If the parameter value is not found, return NA
-      if (length(value_row) == 0) {
+      if(length(value_row) == 0){
+        message("Label not found in data frame.")
         return(NA)
       }
-
       # Search for the next blank row after the value
-      next_blank <- which(apply(reportfile, 1, function(row) all(is.na(row) | row == "" | row == "-" | row == "#")) & (seq_len(nrow(reportfile)) > value_row))[1]
-      # **Add additional situation where there is no blank but a "-" or some other marker spacing values**
+      next_blank <- which(apply(dat, 1, function(row) all(is.na(row) | row == "" | row == "-" | row == "#")) & (seq_len(nrow(dat)) > value_row))[1]
 
-      return(c(value_row, next_blank))
+      # Combine the rows surrounding the selected metric from the output table
+      rows <- c(value_row, next_blank)
+
+      # Extract the metric using the rows from above as a guide and clean up empty columns
+      clean_df <- dat[rows[1]:(rows[2]-1),] |>
+        naniar::replace_with_na_all(condition = ~.x == "")
+      clean_df <- Filter(function(x)!all(is.na(x)), clean_df)
+
+      return(clean_df)
     }
-  }
+
+    # Estimated and focal parameters to put into reformatted output df - naming conventions from SS3
+    param.names <-c()
+    # Loop for all identified parameters to extract for plotting and use
+    for(i in 1:length(param.names)){
+
+    } # close loop
+  } # close SS if statement
 
   if (model == "bam") {
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -86,7 +136,7 @@ convert_output <- function(
     # Specify what to do with the pulled data
     # Might need to rename some values
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  } # close if output is from BAM
+  } # close BAM if statement
 
   if (model == "asap") {
     # This is how Bai read the ASAP output
@@ -94,5 +144,5 @@ convert_output <- function(
     # asap_output <- dget(file.path(casedir, "output", subdir, paste("s", keep_sim_id[om_sim], sep = ""), "asap3.rdat"))
     # setwd(file.path(casedir, "output", subdir, paste("s", keep_sim_id[om_sim], sep = "")))
     # asap_std <- readRep("asap3", suffix = ".std")
-  }
-}
+  } # close asap if statement
+} # close function
