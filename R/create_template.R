da35012957fb700cba1d8abdefb95a9d9422644a
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-08-06 08:58:55 -0400

    replacing format customization in yaml section with function

--- R/create_template.R
+++ R/create_template.R
@@ -343,42 +343,10 @@ create_template <- function(
 
       # Formatting
 
-      if(format == "pdf" | format == "html"){
-
-        if (include_affiliation) {
-          yaml <- paste(yaml, "format: \n",
-            "  ", format, ": \n",
-            "  ", "  ", "toc: ", "true \n",
-            "  ", "  ", "keep-tex: ", "true \n",
-            "  ", "  ", "template-partials: \n",
-            # "  ", "  ", "  ", " - graphics.tex \n",
-            "  ", "  ", "  ", " - title.tex \n",
-            "  ", "  ", "include-in-header: \n",
-            "  ", "  ", "  ", " - in-header.tex \n",
-            sep = ""
-          )
-        } else {
-          yaml <- paste0(
-            yaml, "format: \n",
-            "  ", format, ": \n",
-            "  ", "  ", "toc: ", "true \n",
-            "  ", "  ", "template-partials: \n",
-            "  ", "  ", "  ", "- title.tex \n",
-            "  ", "  ", "keep-tex: true \n"
-          )
-        }
-      } else if (format == "docx") {
-        yaml <- paste0(
-          yaml, "format: \n",
-          "  ", format, ": \n",
-          "  ", "  ", "toc: ", "true \n",
-          "  ", "  ", "template-partials: \n",
-          "  ", "  ", "  ", "- title.tex \n",
-          "  ", "  ", "keep-tex: true \n"
-        )
-      } else {
-        stop("Invalid render format.")
-      }
+      yaml <- paste0(yaml,
+                     format_quarto(format = format,
+                                   include_affiliation = include_affiliation),
+                     "\n")
 
       # Add lua filters for compliance
       # PLACEHOLDER: Uncomment once .lua text is built
