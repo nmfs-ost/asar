6cf807ef6074ef4895b1ff90fb33e4ad4866294e
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-10-04 15:10:07 -0400

    Clarified argument definitions in create_template. Since associated scripts (add_section, create_title, format_quarto) used same parameters (but with different names), changed the parameter names in associated scripts to names in create_template, then inherited parameter names from create_template to reduce redundancy.
    Questions for Sam:
    Please check/edit the "parameters" parameter's definition. What does it mean, and how should it be defined? Is it ever false?
    
    Please check/edit the "param_values" parameter's definition. What does it mean, and how should it be defined?
    
    Please check/edit the "convert_output" parameter's definition for accuracy.

--- R/add_section.R
+++ R/add_section.R
@@ -1,18 +1,6 @@
 #' Add New Section or Subsection to Template
 #'
-#' @param sec_names Names of section(s) (e.g., introduction, results) or
-#' subsection(s) (e.g., a section within the introduction) that will be
-#' added to the document. Please make a short list if >1 section/subsection
-#' will be added. The template will be created as a quarto document, added
-#' into the skeleton, and saved for reference.
-#' @param location Where the section(s)/subsection(s) will be added to
-#' the skeleton template. Please use the notation of 'placement-section'.
-#' For example, 'in-introduction' signifies that the new content would
-#' be created as a child document and added into the 02_introduction.qmd.
-#' To add >1 (sub)section, make the location a list corresponding to the
-#' order of (sub)section names listed in the 'sec_names' parameter.
-#' @param other_sections List of other sections in which the added (sub)sections
-#' will be added (if not NULL).
+#' @inheritParams create_template.R
 #' @param subdir Directory where the new sections will be saved. In the
 #' create_template function, this defaults to the location where the
 #' template is saved.
@@ -24,34 +12,34 @@
 #' @export
 #'
 #' @examples add_section(
-#'   sec_names = "Ecosystem Considerations", location = "after-discussion",
-#'   other_sections = c("introduction.qmd", "model.qmd", "results.qmd", "discussion.qmd"),
+#'   new_section = "Ecosystem Considerations", section_location = "after-discussion",
+#'   custom_sections = c("introduction.qmd", "model.qmd", "results.qmd", "discussion.qmd"),
 #'   subdir = tempdir()
 #' )
 add_section <- function(
-    sec_names = NULL,
-    location = NULL,
-    other_sections = NULL,
+    new_section = NULL,
+    section_location = NULL,
+    custom_sections = NULL,
     subdir = NULL) {
   # Location options
   # before-section
   # after-section
   # in-section (will always append to the end of the section)
-  for (i in 1:length(sec_names)) {
-    section_i_name <- paste0(gsub(" ", "_", sec_names[i]), ".qmd")
-    local_section <- forstringr::str_extract_part(location[i], "-", before = FALSE)
-    local_section_prev <- forstringr::str_extract_part(location[i - 1], "-", before = FALSE)
+  for (i in 1:length(new_section)) {
+    section_i_name <- paste0(gsub(" ", "_", new_section[i]), ".qmd")
+    local_section <- forstringr::str_extract_part(section_location[i], "-", before = FALSE)
+    local_section_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = FALSE)
 
-    if (any("TRUE" %in% grepl("-", location))) {
-      locality <- forstringr::str_extract_part(location[i], "-", before = TRUE)
-      locality_prev <- forstringr::str_extract_part(location[i - 1], "-", before = TRUE)
-    } else if (any("TRUE" %in% grepl(" ", location))) {
-      locality <- forstringr::str_extract_part(location[i], " ", before = TRUE)
-      locality_prev <- forstringr::str_extract_part(location[i - 1], "-", before = TRUE)
+    if (any("TRUE" %in% grepl("-", section_location))) {
+      locality <- forstringr::str_extract_part(section_location[i], "-", before = TRUE)
+      locality_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = TRUE)
+    } else if (any("TRUE" %in% grepl(" ", section_location))) {
+      locality <- forstringr::str_extract_part(section_location[i], " ", before = TRUE)
+      locality_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = TRUE)
     }
 
     section_i <- paste0(
-      "## ", stringr::str_to_title(sub("_", " ", sec_names[i])), "\n",
+      "## ", stringr::str_to_title(sub("_", " ", new_section[i])), "\n",
       "\n",
       "[Insert text here]", "\n",
       "\n",
@@ -60,12 +48,12 @@ add_section <- function(
     utils::capture.output(cat(section_i), file = paste0(subdir, "/", section_i_name), append = FALSE)
 
     if (locality == "before") {
-      other_sections <- append(other_sections, section_i_name, after = (which(grepl(local_section, other_sections)) - 1))
+      custom_sections <- append(custom_sections, section_i_name, after = (which(grepl(local_section, custom_sections)) - 1))
     } else if (locality == "after") {
       if (locality %in% locality_prev & local_section %in% local_section_prev) {
-        other_sections <- append(other_sections, section_i_name, after = which(grepl(gsub(" ", "_", sec_names[i - 1]), other_sections)))
+        custom_sections <- append(custom_sections, section_i_name, after = which(grepl(gsub(" ", "_", new_section[i - 1]), custom_sections)))
       } else {
-        other_sections <- append(other_sections, section_i_name, after = which(grepl(local_section, other_sections)))
+        custom_sections <- append(custom_sections, section_i_name, after = which(grepl(local_section, custom_sections)))
       }
     } else if (locality == "in") {
       stop("No available option for adding a new section 'in' another quarto document.", call. = FALSE)
@@ -73,5 +61,5 @@ add_section <- function(
       stop("Invalid selection for placement of section. Please name the follow the format 'placement-section_name' for adding a new section.")
     }
   }
-  other_sections
+  custom_sections
 }
