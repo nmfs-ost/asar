f92cbe959a8dacad2bb9fab34928ea71b635125d
Bai-Li-NOAA <Bai-Li-NOAA@users.noreply.github.com>
2024-10-07 15:50:37 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- tests/testthat/test-convert_output.R
+++ tests/testthat/test-convert_output.R
@@ -1,5 +1,4 @@
 test_that("convert_output works for SS3", {
-
   # Get a list of directories containing SS3 models
   all_models <- list.dirs(
     file.path(test_path("fixtures", "ss3_models"), "models"),
@@ -8,8 +7,7 @@ test_that("convert_output works for SS3", {
   )
 
   # Loop through each model directory and check convert_output function
-  for (i in seq_along(all_models)){
-
+  for (i in seq_along(all_models)) {
     # Ensure no errors occur while converting SS3 output
     expect_no_error(result <- convert_output(
       output_file = "Report.sso",
@@ -40,5 +38,4 @@ test_that("convert_output works for SS3", {
 
   # Ensure the CSV file was created successfully
   expect_true(file.exists(file.path(tempdir(), "std_model_output.csv")))
-
 })
