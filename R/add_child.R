b63c1ae82ed727c50ef861b5502ce89dedb52386
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-10-10 16:54:26 -0400

    build: updating/adding examples to functions (add_base_section.R, add_child.R, add_chunk.R).

--- R/add_child.R
+++ R/add_child.R
@@ -1,13 +1,21 @@
 #' Write R Chunk to Add Child Document
 #'
-#' @param x An additional section to add into the template. Options for additional sections are in the 'skeleton' folder. Appropriate files are .qmd files and are formatted as such: XX_section.qmd (i.e., not a, b, c... subfiles).
-#' @param label Description of the child document being added. It should be short- one or two words, maximum.
+#' @param x An additional section to add into the template. Options
+#' for additional sections are in the 'skeleton' folder.
+#' Appropriate files are .qmd files and are formatted as such:
+#' XX_section.qmd (i.e., not a, b, c... subfiles).
+#' @param label Description of the child document being added.
+#' It should be short- one or two words, maximum.
 #'
-#' @return Formatting R chunk for child document to add section into the template/skeleton.
-#'         Utilize the cat() function to implement into readable text.
+#' @return Formatting R chunk for child document to add section
+#' into the template/skeleton. Utilize the cat() function to
+#' implement into readable text.
 #' @export
 #'
-#' @examples add_child("test_quarto.qmd", label = "test_doc")
+#' @examples
+#'\dontrun{
+#' add_child(x = "test_quarto.qmd", label = "test_doc")
+#' }
 add_child <- function(x,
                       label = NULL) {
   child <- c()
