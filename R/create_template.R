d6fcf1f9c84039b62d00d97540e596a79a01594d
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2024-10-24 15:07:27 -0400

    Create working example and resources for users (#54)
    * feat(README): change example to petrale
    
    Changing to a working example in read me that mimic that of a current stock assessment. The files resulting from the example and rendered report need to be added as their own example in a folder
    
    * update(example): change README so users can run  petrale example
    
    # Summary
    
    - add raw and converted petrale output to data folder
    - ran usethis::use_data() creating structure for the example
    - updated documentation
    
    * fix:(create_template): add preamble
    
    - read in converted output in chunk
    - create place for pre-loaded quantities to reference later in the document
    
    * update(test-add_chunk): correct notes in test
    
    * Fix(readme): example needed csv file
    
    * Update(readme): adjust for new section names
    
    * update(website): change set up of pkgdown website
    
    * Update(pkgdown.yml): include tutorial
    
    * feature(add tutorial): demo in shiny app
    
    Might need to move folders - pushing now in order to switch branches ok
    
    * remove(tutorial): move materials to asar-demo repo and host there
    
    * update(pkgdown): add tutorial link to site
    
    * update(README): add link for tutorial in examples
    
    * Update(pkgdown yml): Reorder top nav bar and have a drop down manual for all vignettes instead
    
    * Remove example data - moved to asar-demo
    
    * fix(create_template): replaced params_chunk with preamble

--- R/create_template.R
+++ R/create_template.R
@@ -651,12 +651,17 @@ create_template <- function(
 
       # print("_______Standardized output data________")
 
-      # Add in quantities and data R chunk
-      params_chunk <- add_chunk(
-        glue::glue(
-          "output <- read.csv('", resdir, "/", model_results, "') \n \n",
-          "# Reference quantities \n",
-          "# sbtgt \n"
+      # Add preamble
+      # add in quantities and output data R chunk
+      preamble <- add_chunk(
+        paste0(
+          "output <- utils::read.csv('",
+          ifelse(convert_output,
+                 paste0(subdir, "/", species, "_",  "_std_res_", year, ".csv"),
+                 paste0(resdir, "/", model_results)), "') \n",
+          "# Call reference points and quantities below \n",
+          "# sbmsy = 10000 \n",
+          "# fmsy = 0.3 \n"
         ),
         label = "output_and_quantities"
       )
@@ -784,7 +789,7 @@ create_template <- function(
       # Combine template sections
       report_template <- paste(
         yaml,
-        params_chunk,
+        preamble,
         citation,
         sections,
         sep = "\n"
@@ -940,7 +945,7 @@ create_template <- function(
       # Combine template sections
       report_template <- paste(
         yaml,
-        # ass_output,
+        # preamble,
         citation,
         sections,
         sep = "\n"
