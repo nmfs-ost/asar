28a7606cbdc24ecb3fcf52b1a7a552d20a2d44cf
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-11-04 16:21:53 -0500

    continuing to troubleshoot

--- R/create_template.R
+++ R/create_template.R
@@ -697,12 +697,15 @@ create_template <- function(
       # Create a chunk that imports csv with full captions and alt text
       # created by satf
 
-      if (used_satf == F) {
+      if (used_satf) {
+        caps_and_alt_text <- read.csv('captions_alt_text.csv')
+      } else {
         satf::write_captions(output)
-       }
+        caps_and_alt_text <- read.csv('captions_alt_text.csv')
+      }
 
       add_captions <- add_chunk(
-        paste0(
+        print(
           ifelse(used_satf,
                  # the user HAS already generated captions with satf
                  "caps_and_alt_text <- read.csv('captions_alt_text.csv')",
@@ -996,7 +999,7 @@ create_template <- function(
       report_template <- paste(
         yaml,
         # preamble,
-        add_captions,
+        # add_captions,
         citation,
         sections,
         sep = "\n"
