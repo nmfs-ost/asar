6f7c168b476771d77b5636cd19206d03ca1e0146
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-11-04 14:48:51 -0500

    putting alt text-related code back into script for troubleshooting

--- R/create_template.R
+++ R/create_template.R
@@ -88,6 +88,9 @@
 #'  name of the file, can be used (e.g., 'abstract' rather than
 #'  '00_abstract.qmd'). If adding a new section, also use
 #'   parameters 'new_section' and 'section_location'.
+#' @param used_satf TRUE/FALSE; has the user already used the satf
+#' package to generate figures, tables, and therefore figure/table
+#' captions and alt text? Default is false.
 #' @param include_figures TRUE/FALSE; Should figures be
 #' included in the report? Default is true.
 #' @param include_tables TRUE/FALSE; Should tables be included
@@ -175,6 +178,7 @@
 #'   prev_year = 2021,
 #'   custom = TRUE,
 #'   custom_sections = c("executive_summary", "introduction", "discussion"),
+#'   used_satf = FALSE,
 #'   include_figures = TRUE,
 #'   include_tables = TRUE,
 #'   add_image = TRUE,
@@ -212,7 +216,6 @@ create_template <- function(
     prev_year = NULL,
     custom = FALSE,
     custom_sections = NULL,
- #   used_satf = FALSE,
     include_figures = TRUE,
     include_tables = TRUE,
     add_image = FALSE,
@@ -401,8 +404,7 @@ create_template <- function(
       # Pull authors and affiliations from national db
       # Parameters to add authorship to YAML
       # Read authorship file
-      authors <- utils::read.csv(system.file("resources", "authorship.csv",
-                                             package = "asar", mustWork = TRUE)) |>
+      authors <- utils::read.csv(system.file("resources", "authorship.csv", package = "asar", mustWork = TRUE)) |>
         dplyr::mutate(
           mi = dplyr::case_when(
             mi == "" ~ NA,
@@ -652,11 +654,10 @@ create_template <- function(
             file_save = TRUE,
             model = model,
             savedir = subdir,
-            save_name = paste(species, "_std_res_", year, sep = "")
+            save_name = paste(sub(" ", "_", species), "_std_res_", year, sep = "")
           )
         }
-        # Rename model results file and results file directory if the results
-        # are converted in this fxn
+        # Rename model results file and results file directory if the results are converted in this fxn
         model_results <- glue::glue(species, "_std_res_", year, ".csv")
         resdir <- subdir
       }
@@ -669,8 +670,8 @@ create_template <- function(
         paste0(
           "output <- utils::read.csv('",
           ifelse(convert_output,
-                 filepath(subdir, paste0(species, "_std_res_", year, ".csv")),
-                 filepath(resdir, model_results)), "') \n \n",
+                 paste0(subdir, "/", species, "_std_res_", year, ".csv"),
+                 paste0(resdir, "/", model_results)), "') \n",
           "# Call reference points and quantities below \n",
           "# start_year <- min(output$year) \n",
           "# end_year <- '", year, "' \n",
@@ -691,7 +692,28 @@ create_template <- function(
         label = "output_and_quantities"
       )
 
+
+
       # Create a chunk that imports csv with full captions and alt text
+      # created by satf
+
+      if (used_satf == F) {
+        satf::write_captions(output)
+       }
+
+      add_captions <- add_chunk(
+        paste0(
+          ifelse(used_satf,
+                 # the user HAS already generated captions with satf
+                 "caps_and_alt_text <- read.csv('captions_alt_text.csv')",
+                 # the user has NOT already generated captions with satf
+                 "satf::write_captions(output) \n caps_and_alt_text <- read.csv('captions_alt_text.csv')"
+          )
+        )
+        ,
+        label = "captions_and_alt_text"
+      )
+
 
       # Add page for citation of assessment report
       citation <- create_citation(
@@ -817,6 +839,7 @@ create_template <- function(
       report_template <- paste(
         yaml,
         preamble,
+        add_captions,
         citation,
         sections,
         sep = "\n"
@@ -973,6 +996,7 @@ create_template <- function(
       report_template <- paste(
         yaml,
         # preamble,
+        add_captions,
         citation,
         sections,
         sep = "\n"
