95920f7f47fade5da7d5c44b36a7d33786fb03ac
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-11-26 09:35:51 -0500

    updated labels to fig-__ and tbl-__ in create_figures/tables_docs; updated gitignore

--- R/create_figures_doc.R
+++ R/create_figures_doc.R
@@ -31,7 +31,7 @@ create_figures_doc <- function(resdir = NULL,
       figures_doc,
       add_chunk(
         paste0("satf::plot_recruitment(dat = output)"),
-        label = "recruitment",
+        label = "fig-recruitment",
         eval = "false",
         add_option = TRUE,
         chunk_op = c(
@@ -64,7 +64,7 @@ create_figures_doc <- function(resdir = NULL,
     figures_doc <- paste0(
       figures_doc,
       add_chunk(plot_code,
-        label = "spawn_bio",
+        label = "fig-spawn_bio",
         eval = "false",
         add_option = TRUE,
         chunk_op = c(
--- R/create_tables_doc.R
+++ R/create_tables_doc.R
@@ -30,7 +30,7 @@ create_tables_doc <- function(resdir = NULL,
         paste0(
           "satf::table_indices(dat = output)"
         ),
-        label = "indices",
+        label = "tbl-indices",
         eval = "false",
         add_option = TRUE,
         chunk_op = c(
