385a5cd5ccc12810127741c93ce74782bb44b14e
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-12-05 17:14:55 -0500

    fix(convert_output): exp and obs labels are properly indicated whether the source is from indices or catch
    - ss3 and bam naming conventions are now merged by module_name as well
    - module_name is not an essential and non-changeable column

--- R/convert_output.R
+++ R/convert_output.R
@@ -244,7 +244,7 @@ convert_output <- function(
     errors <- c("StdDev", "sd", "se", "SE", "cv", "CV")
     miss_parms <- c()
     out_list <- list()
-    ### SS3 loop ####
+    #### SS3 loop ####
     for (i in 1:length(param_names)) {
       # Processing data frame
       parm_sel <- param_names[i]
@@ -747,6 +747,7 @@ convert_output <- function(
     # argument for function when model == BAM
     # fleet_names <- c("cl", "cL","cp","mrip","ct", "hb", "HB", "comm","Mbft","CVID")
 
+    ##### BAM loop ----
     # Extract data from list fit to output df
     # Loop over all items to output each object/transform
     # Not transforming or inclusing info chunk
@@ -1146,7 +1147,7 @@ convert_output <- function(
         warning(paste(names(extract), " not compatible.", sep = ""))
       } # close if statement
     } # close loop over objects listed in dat file
-    #### WHAM ####
+    #### WHAM ----
   } else if (model == "wham") {
     # This is how Bai read the ASAP output
     # asap_output conversion written by Bai Li
@@ -1181,7 +1182,7 @@ convert_output <- function(
     var_names_sheet <- openxlsx::read.xlsx(con_file)
   }
   if (file.exists(con_file)) {
-    out_new <- dplyr::inner_join(out_new, var_names_sheet, by = "label") |>
+    out_new <- dplyr::inner_join(out_new, var_names_sheet, by = c("module_name","label")) |>
       dplyr::mutate(label = dplyr::case_when(
         !is.na(alt_label) ~ alt_label,
         TRUE ~ label
