612d20123814156fa7143a82735268ba5b7824b0
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-12-10 10:59:33 -0500

    Feature(add package rd): adds documentation for package and removes global variables in checks

---
+++ R/asar-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+globvar <- c("yr","value","estimate","seas","subseas","age_bins","morph",
+             "age","year","nsim","fleet","uncertainty","initial","alt_label",
+             "last","affiliation","label","type","caption","alt_text","caption",
+             "name",".")
+if(getRversion() >= "2.15.1")  utils::globalVariables(globvar)
