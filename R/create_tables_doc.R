d9168fbe1d3c2e5523cc33e70d39c9f38b61322a
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-12-12 10:03:44 -0500

    changed eval for first chunk to true, and eval for indices table setup chunk to false, re: https://github.com/nmfs-ost/asar/issues/102 . Will change eval to true for all chunks once indices table fxn is completed.

--- R/create_tables_doc.R
+++ R/create_tables_doc.R
@@ -29,8 +29,8 @@ create_tables_doc <- function(resdir = NULL,
       add_chunk(
         paste0("rda_dir <- '", rda_dir, "/rda_files'"),
         label = "set-rda-dir-tbls",
-        eval = "false"
-      ),
+        eval = "true"
+        ),
       "\n"
     )
 
@@ -49,7 +49,7 @@ rm(rda)\n
 indices_table <- indices_table_rda$table
 indices_cap <- indices_table_rda$cap"),
         label = "tbl-indices-setup",
-        eval = "true"
+        eval = "false"
       ),
       "\n"
     )
