10cd2351bb1fcb6b93891754c91c081c10948536
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-12-31 14:36:40 -0500

    Added welcome message inviting feedback

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
+### Start up messages and actions to run for use of satf
+# Start up message
+
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Thanks for using asar! Please visit this GitHub Discussions page to offer feedback on the package: https://github.com/nmfs-ost/asar/discussions/categories/feedback-for-v1-0.")
+}
+
 # Things to load on start up
 # .onLoad <- function(libname, pkgname){
 #   # install satf
