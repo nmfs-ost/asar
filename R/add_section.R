2841dcd3f7ea3cc3d73ae04300c3c5da0ca43180
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2025-01-21 14:37:09 -0500

    Custom template fixes (#143)
    * update(create_template): comment out skeleton pop up at the end
    
    * update(add_section): add functionality for putting new subsection into section
    
    * Update(add_section): fix pathing for locating section to place code and change from add_chunk to add_child
    
    * update(create_template): select only custom_sections when user wants to make a different outline of report
    
    custom must = TRUE otherwise copies all files
    
    * Fix(create_template): adjust if statement argument for custom_sections
    
    * Add nmfspalette package back in
    
    * removed add_theme and associated test and documentation
    
    * remove static NOAA Fishies in authorship
    
    * update NOAA fisheries affiliations
    
    Co-authored-by: Schiano-NOAA <Schiano-NOAA@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sbreitbart-NOAA <sophie.breitbart@noaa.gov>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Schiano-NOAA <Schiano-NOAA@users.noreply.github.com>

--- R/add_section.R
+++ R/add_section.R
@@ -8,7 +8,9 @@
 #' @return Add an additional section or subsection to the report template
 #' if it is not already present in the default template. This provides
 #' the option to add it as a section before or after an existing section,
-#' or within a section as a child document.
+#' or within a section as a child document. For developers: this function
+#' creates a list of sections that will be added to the skeleton file
+#' made from create_template.
 #' @export
 #'
 #' @examples add_section(
@@ -20,6 +22,7 @@ add_section <- function(
     new_section = NULL,
     section_location = NULL,
     custom_sections = NULL,
+    custom = TRUE,
     subdir = NULL) {
   # Location options
   # before-section
@@ -27,15 +30,27 @@ add_section <- function(
   # in-section (will always append to the end of the section)
   for (i in 1:length(new_section)) {
     section_i_name <- paste0(gsub(" ", "_", tolower(new_section[i])), ".qmd")
-    local_section <- forstringr::str_extract_part(section_location[i], "-", before = FALSE)
-    local_section_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = FALSE)
+    local_section <- forstringr::str_extract_part(
+      section_location[i], "-", before = FALSE
+    )
+    local_section_prev <- forstringr::str_extract_part(
+      section_location[i - 1], "-", before = FALSE
+    )
 
     if (any("TRUE" %in% grepl("-", section_location))) {
-      locality <- forstringr::str_extract_part(section_location[i], "-", before = TRUE)
-      locality_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = TRUE)
+      locality <- forstringr::str_extract_part(
+        section_location[i], "-", before = TRUE
+      )
+      locality_prev <- forstringr::str_extract_part(
+        section_location[i - 1], "-", before = TRUE
+      )
     } else if (any("TRUE" %in% grepl(" ", section_location))) {
-      locality <- forstringr::str_extract_part(section_location[i], " ", before = TRUE)
-      locality_prev <- forstringr::str_extract_part(section_location[i - 1], "-", before = TRUE)
+      locality <- forstringr::str_extract_part(
+        section_location[i], " ", before = TRUE
+      )
+      locality_prev <- forstringr::str_extract_part(
+        section_location[i - 1], "-", before = TRUE
+      )
     }
 
     section_i <- paste0(
@@ -45,21 +60,52 @@ add_section <- function(
       "\n",
       add_chunk("# Insert code", label = "example_chunk"), "\n"
     )
-    utils::capture.output(cat(section_i), file = paste0(subdir, "/", section_i_name), append = FALSE)
+    utils::capture.output(
+      cat(section_i),
+      file = paste0(subdir, "/", section_i_name),
+      append = FALSE
+    )
 
     if (locality == "before") {
-      custom_sections <- append(custom_sections, section_i_name, after = (which(grepl(local_section, custom_sections)) - 1))
+      custom_sections <- append(
+        custom_sections,
+        section_i_name,
+        after = (which(grepl(local_section, custom_sections)) - 1)
+      )
     } else if (locality == "after") {
       if (locality %in% locality_prev & local_section %in% local_section_prev) {
-        custom_sections <- append(custom_sections, section_i_name, after = which(grepl(gsub(" ", "_", tolower(new_section[i - 1])), custom_sections)))
+        custom_sections <- append(
+          custom_sections,
+          section_i_name,
+          after = which(grepl(
+            gsub(" ", "_", tolower(new_section[i - 1])),
+            custom_sections)
+          )
+        )
       } else {
-        custom_sections <- append(custom_sections, section_i_name, after = max(which(grepl(local_section, custom_sections))))
+        custom_sections <- append(
+          custom_sections,
+          section_i_name,
+          after = max(which(grepl(local_section, custom_sections)))
+        )
       }
     } else if (locality == "in") {
-      stop("No available option for adding a new section 'in' another quarto document.", call. = FALSE)
+       # stop("No available option for adding a new section 'in' another quarto document.", call. = FALSE)
+        # recognize locality_prev file
+        file_for_subsection <- list.files(file.path(subdir))[grep(local_section, list.files(file.path(subdir)))]
+        # create code for reading in child doc
+        child_sec <- add_child(
+          section_i_name,
+          label = gsub(" ", "_", tolower(new_section[i]))
+        )
+        # append that text to file
+        utils::capture.output(cat(child_sec), file = fs::path(subdir, file_for_subsection), append = TRUE)
+        # section does not need to be added to appended custom sections as stated above
+        # creating qmd is already done in line 48
+      
     } else {
       stop("Invalid selection for placement of section. Please name the follow the format 'placement-section_name' for adding a new section.")
     }
-  }
+  } # close for loop
   custom_sections
 }
