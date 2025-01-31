85c6956e0347feb7fe8f98b3c1f12b0d6bb00f19
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-01-31 13:22:43 -0500

    update(test-alttext): change test for alttext to mimic changes to the function - sdoes not work still

--- tests/testthat/test-add_alttext.R
+++ tests/testthat/test-add_alttext.R
@@ -35,61 +35,62 @@
 #   # render template
 #   quarto::quarto_render(file.path(path, "report", "SAR_USWC_Simple_skeleton.qmd"))
 #
-#   # # run add_tagging and check if new one is created
-#   # withr::with_dir(
-#   #   file.path(getwd(), "report"),
-#   #   add_accessibility(
-#   #     x = "SAR_USWC_Simple_skeleton.tex",
-#   #     dir = getwd(),
-#   #     rda_dir = path,
-#   #     compile = TRUE,
-#   #     rename = "Simple_SAR_2025_tagged"
-#   #   )
-#   # )
+#   # run add_tagging and check if new one is created
+#   withr::with_dir(
+#     file.path(getwd(), "report"),
+#     add_alttext(
+#       x = "SAR_USWC_Simple_skeleton.tex",
+#       dir = getwd(),
+#       rda_dir = path,
+#       compile = TRUE,
+#       alttext_csv_dir = path,
+#       rename = "Simple_SAR_2025_alttext"
+#     )
+#   )
+#
 #   # Extract file to compare to snapshot
-#   compare_file <- readLines(file.path(getwd(), test_path(), "report", "Simple_SAR_2025_tagged.tex"))
+#   # compare_file <- readLines(file.path(path, "report", "Simple_SAR_2025_alttext.tex"))
 #
 #   # TODO: make check to be expect equal by reading the latex as string
 #   # and make an expectation to specific line
 #   # Make wrapper for expect_snapshot_file
-#   expect_snapshot_tex <- function(tex_file, tex_dir, rename, path) {
-#     # Other packages might affect results
-#     skip_if_not_installed("tinytex")
-#     # Or maybe the output is different on some operation systems
-#     # skip_on_os("windows")
-#     # You'll need to carefully think about and experiment with these skips
-#
-#     # tex_file <- glue::glue("{name}.txt")
-#     announce_snapshot_file(name = tex_file)
-#
-#     path <- withr::with_dir(
-#       file.path(path, "report"),
-#       add_alttext(
-#         x = tex_file,
-#         dir = tex_dir,
-#         rda_dir = path,
-#         compile = TRUE,
-#         rename = rename
-#       )
-#     )
-#     testthat::expect_snapshot_file(
-#       path = path,
-#       name = tex_file)
-#   }
-#
-#   expect_snapshot_tex(
-#     tex_file = "SAR_USWC_Simple_skeleton.tex",
-#     tex_dir = getwd(),
-#     rename = "Simple_SAR_2025_tagged",
-#     path = path
-#   )
-#
-#   # expect_snapshot_file(
-#   #   compare_file_text(
-#   #     readLines(file.path(getwd(), "report", "SAR_USWC_Simple_skeleton.tex")),
-#   #     readLines(file.path(getwd(), "tests", "testthat", "snaps", "Simple_SAR_2025_tagged.tex"))
+#   # expect_snapshot_tex <- function(tex_file, tex_dir, rename, path) {
+#   #   # Other packages might affect results
+#   #   skip_if_not_installed("tinytex")
+#   #   # Or maybe the output is different on some operation systems
+#   #   # skip_on_os("windows")
+#   #   # You'll need to carefully think about and experiment with these skips
+#   #
+#   #   # tex_file <- glue::glue("{name}.txt")
+#   #   announce_snapshot_file(path = file.path(path, "report"),
+#   #                          name = "Simple_SAR_2025_alttext.tex")
+#   #
+#   #   path <- withr::with_dir(
+#   #     file.path(path, "report"),
+#   #     add_alttext(
+#   #       x = tex_file,
+#   #       dir = tex_dir,
+#   #       rda_dir = path,
+#   #       compile = TRUE,
+#   #       rename = rename
+#   #     )
 #   #   )
+#   #   testthat::expect_snapshot_file(
+#   #     path = path,
+#   #     name = tex_file)
+#   # }
+#   #
+#   # expect_snapshot_tex(
+#   #   tex_file = "SAR_USWC_Simple_skeleton.tex",
+#   #   tex_dir = getwd(),
+#   #   rename = "Simple_SAR_2025_tagged",
+#   #   path = path
 #   # )
+#
+#   expect_snapshot_file(
+#     path = file.path(path, "report"),
+#     name = "Simple_SAR_2025_alttext.tex"
+#   )
 #
 #   # Remove testing files
 #   on.exit(unlink(file.path(getwd(), "report"),
