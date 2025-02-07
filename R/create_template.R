808ee43859ed462e89c8f49e1e6e1e38ae94bbc2
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-02-07 09:28:41 -0500

    update terminal sb naming to match other conventions

--- R/create_template.R
+++ R/create_template.R
@@ -922,7 +922,7 @@ create_template <- function(
           "  ", "  ", "is.na(age)) |>", "\n",
           "  ", "dplyr::pull(estimate)", "\n",
           "# spawning biomass in the last year\n",
-          "sbend <- output2 |>", "\n",
+          "SBend <- output2 |>", "\n",
           "  ", "dplyr::filter(grepl('spawning_biomass', label), year == end_year) |>", "\n",
           "  ", "dplyr::pull(estimate) |>", "\n",
           "  ", "  ", "unique()", "\n",
