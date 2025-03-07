a9e59cc940173a29d2ce6a33a747498f60f37809
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-03-07 10:26:13 -0500

    fix(tables and figs docs): remove extra # indicating the wrong heading level for the tables.qmd and figures.qmd docs when rendered without any tables and figures

--- R/create_template.R
+++ R/create_template.R
@@ -579,7 +579,7 @@ create_template <- function(
           }
         } else {
           tables_doc <- paste0(
-            "### Tables \n \n",
+            "## Tables \n \n",
             "Please refer to the `stockplotr` package downloaded from remotes::install_github('nmfs-ost/stockplotr') to add premade tables."
           )
           utils::capture.output(cat(tables_doc), file = fs::path(subdir, "08_tables.qmd"), append = FALSE)
@@ -613,7 +613,7 @@ create_template <- function(
           }
         } else {
           figures_doc <- paste0(
-            "### Figures \n \n",
+            "## Figures \n \n",
             "Please refer to the `stockplotr` package downloaded from remotes::install_github('nmfs-ost/stockplotr') to add premade figures."
           )
           utils::capture.output(cat(figures_doc), file = fs::path(subdir, "09_figures.qmd"), append = FALSE)
