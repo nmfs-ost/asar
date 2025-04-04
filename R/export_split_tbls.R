517a66ff9ac3f76b8b08677eaa397195632448a1
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-04-04 15:55:48 -0400

    moved ID_tbl_width_class() and export_split_tbls() from utils to own functions; also changed add_alttext.r to .R filetype

---
+++ R/export_split_tbls.R
@@ -0,0 +1,39 @@
+#' Split extra-wide tables into smaller tables and export
+#'
+#' @inheritParams render_lg_table
+#'
+#' @return The number of split tables
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' export_split_tbls(
+#'   rda_dir = here::here(),
+#'   plot_name = "bnc_table.rda",
+#'   essential_columns = 5
+#' )
+#'
+#' export_split_tbls(
+#'   rda_dir = getwd(),
+#'   plot_name = "indices.abundance_table.rda",
+#'   essential_columns = 1:2
+#' )
+#' }
+export_split_tbls <- function(
+    rda_dir = NULL,
+    plot_name = NULL,
+    essential_columns = NULL) {
+  rda_path <- file.path(paste0(rda_dir, "/rda_files/", plot_name))
+
+  load(rda_path)
+  table_rda <- rda
+  rm(rda)
+
+  # split tables and export
+  render_lg_table(
+    report_flextable = table_rda$table,
+    essential_columns = essential_columns,
+    rda_dir = rda_dir,
+    plot_name = plot_name
+  )
+}
