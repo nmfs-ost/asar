2623b8cc904b57b48c839482f17270653bfd9850
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-04-07 12:31:02 -0400

    update(a11y fxns): update documentation and add in additional checks to fxns

--- R/add_tagging.r
+++ R/add_tagging.r
@@ -2,18 +2,11 @@
 #'
 #' @inheritParams add_accessibility
 #'
-<<<<<<< HEAD
 #' @return This function was made to help add in
 #' latex packages and content associated with PDF
 #' tagging. Quarto does not allow the user to edit anything
 #' before documentclass, so this function alters the rendered .tex file.
 #' 
-=======
-#' @return DRAFT: This function was made to help add in
-#' latex packages and content associated with PDF
-#' tagging. Quarto does not allow the user to edit anything
-#' before documentclass, so this function alters the rendered .tex file.
->>>>>>> 6b58bb4 (reset branch to dev)
 #' @export
 #'
 #' @examples
@@ -51,15 +44,7 @@ add_tagging <- function(
     compile = TRUE,
     rename = NULL
 ) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   # Read latex file
-=======
-    # Read latex file
->>>>>>> 6b58bb4 (reset branch to dev)
-=======
-  # Read latex file
->>>>>>> a1a1141 (update(a11y fxns): adjust way alt text is added and the tagging function per work done in quarto-accessibility-reprex repo)
   tex_file <- readLines(file.path(dir, x))
 
   # Identify line where the new accessibility content should be added after
