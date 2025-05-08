c5150207ae8354cab1df5dd30d78088c9cdd9b72
Schiano-NOAA <125507018+Schiano-NOAA@users.noreply.github.com>
2025-05-08 15:17:55 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/create_citation.R
+++ R/create_citation.R
@@ -19,10 +19,61 @@ create_citation <- function(
     year = NULL) {
   if (is.null(year)) year <- format(as.POSIXct(Sys.Date(), format = "%YYYY-%mm-%dd"), "%Y")
   # Check if author is input - improved from previous fxn so did not fail
-    if (any(author == "" | is.null(author))) {
-      message("Authorship is not defined.")
-      # Define default citation - needs author editing
-      citation <- paste0(
+  if (any(author == "" | is.null(author))) {
+    message("Authorship is not defined.")
+    # Define default citation - needs author editing
+    citation <- paste0(
+      "{{< pagebreak >}} \n",
+      "\n",
+      "Please cite this publication as: \n",
+      "\n",
+      "[AUTHOR NAME]. [YEAR]. ",
+      title, ". National Marine Fisheries Service, ",
+      "[CITY], [STATE]. "
+    )
+  } else {
+    # Authored by Kelli Johnson in previous PR
+    author_data_frame <- tibble::as_tibble_col(
+      author,
+      column_name = "input"
+    ) |>
+      tidyr::separate_wider_regex(
+        cols = input,
+        # Caitlin Allen Akselrud is the only non-hyphenated dual last name
+        # and needs to be included as its own pattern.
+        # The second pattern allows for first initials rather than first name
+        patterns = c(first = "Caitlin |^[A-Z]. |.*[a-z] ", last = ".*$")
+      ) |>
+      tidyr::separate_wider_delim(
+        cols = last,
+        delim = ". ",
+        names = c("mi", "last"),
+        too_few = "align_end"
+      ) |>
+      dplyr::mutate(
+        first = gsub(" ", "", first),
+        mi = ifelse(is.na(mi), "", paste0(mi, "."))
+      ) |>
+      dplyr::left_join(
+        y = utils::read.csv(
+          system.file(
+            "resources",
+            "authorship.csv",
+            package = "asar",
+            mustWork = TRUE
+          )
+        ),
+        by = c("first", "mi", "last")
+      )
+
+    # Extract location of primary author
+    primary_author_office <- utils::read.csv(system.file("resources", "affiliation_info.csv", package = "asar", mustWork = TRUE)) |>
+      dplyr::filter(affiliation == author_data_frame$office[1])
+
+    # Check
+    if (nrow(primary_author_office) < 1) {
+      warning("No location found for primary author. Please edit the citation found in the 'skeleton.qmd'.")
+      cit <- paste0(
         "{{< pagebreak >}} \n",
         "\n",
         "Please cite this publication as: \n",
@@ -32,130 +83,78 @@ create_citation <- function(
         "[CITY], [STATE]. "
       )
     } else {
-      # Authored by Kelli Johnson in previous PR
-      author_data_frame <- tibble::as_tibble_col(
-        author,
-        column_name = "input"
-      ) |>
-        tidyr::separate_wider_regex(
-          cols = input,
-          # Caitlin Allen Akselrud is the only non-hyphenated dual last name
-          # and needs to be included as its own pattern.
-          # The second pattern allows for first initials rather than first name
-          patterns = c(first = "Caitlin |^[A-Z]. |.*[a-z] ", last = ".*$")
-        ) |>
-        tidyr::separate_wider_delim(
-          cols = last,
-          delim = ". ",
-          names = c("mi", "last"),
-          too_few = "align_end"
-        ) |>
+      # Author naming convention formatting
+      author_list <- author_data_frame |>
         dplyr::mutate(
-          first = gsub(" ", "", first),
-          mi = ifelse(is.na(mi), "", paste0(mi, "."))
+          first_initial = gsub("([A-Z])[a-z]+", "\\1.", first),
+          bib = purrr::pmap(
+            list(x = first_initial, y = mi, z = last),
+            \(x, y, z) toBibtex(person(given = c(x, y), family = z))
+          )
         ) |>
-        dplyr::left_join(
-          y = utils::read.csv(
-            system.file(
-              "resources",
-              "authorship.csv",
-              package = "asar",
-              mustWork = TRUE
-            )
-          ),
-          by = c("first", "mi", "last")
-        )
+        dplyr::pull(bib) |>
+        gsub(pattern = " $", replacement = "") |>
+        glue::glue_collapse(sep = ", ", last = ", and ")
+    }
 
-      # Extract location of primary author
-      primary_author_office <- utils::read.csv(system.file("resources", "affiliation_info.csv", package = "asar", mustWork = TRUE)) |>
-        dplyr::filter(affiliation == author_data_frame$office[1])
+    # Authored by Sam Schiano with contributions from Kelli Johnson
 
-      # Check
-      if (nrow(primary_author_office) < 1) {
-        warning("No location found for primary author. Please edit the citation found in the 'skeleton.qmd'.")
-        cit <- paste0(
-          "{{< pagebreak >}} \n",
-          "\n",
-          "Please cite this publication as: \n",
-          "\n",
-          "[AUTHOR NAME]. [YEAR]. ",
-          title, ". National Marine Fisheries Service, ",
-          "[CITY], [STATE]. "
+    region_specific_part <- switch(primary_author_office[["office"]],
+      "AFSC" = {
+        paste0(
+          "North Pacific Fishery Management Council, Anchorage, AK. Available from ",
+          "https://www.npfmc.org/library/safe-reports/"
         )
-      } else {
-        # Author naming convention formatting
-        author_list <- author_data_frame |>
-          dplyr::mutate(
-            first_initial = gsub("([A-Z])[a-z]+", "\\1.", first),
-            bib = purrr::pmap(
-              list(x = first_initial, y = mi, z = last),
-              \(x, y, z) toBibtex(person(given = c(x, y), family = z))
-            )
-          ) |>
-          dplyr::pull(bib) |>
-          gsub(pattern = " $", replacement = "") |>
-          glue::glue_collapse(sep = ", ", last = ", and ")
-      }
-      
-      # Authored by Sam Schiano with contributions from Kelli Johnson
-      
-      region_specific_part <- switch(
-        primary_author_office[["office"]],
-        "AFSC" = {
-          paste0(
-            "North Pacific Fishery Management Council, Anchorage, AK. Available from ",
-            "https://www.npfmc.org/library/safe-reports/"
-          )
-        },
-        "NWFSC" = {
-          paste0(
-            "Prepared by [COMMITTEE]. [XX] p."
-          )
-        },
-        "SEFSC" = {
-          paste0(
-            "SEDAR, North Charleston SC. [XX] pp. ",
-            "available online at: http://sedarweb.org/"
-          )
-        },
-        "SWFSC" = {
-          paste0(
-            "Pacific Fishery Management Council, Portland, OR. Available from https://www.pcouncil.org/stock-assessments-and-fishery-evaluation-safe-documents/."
-          )
-        },
-        "PIFSC" = {
-          paste0(
-            "NOAA Tech. Memo. [TECH MEMO NUMBER]",
-            ", ", "[XX] p."
-          )
-        },
-        "NEFSC" = {
-          paste0(
-            primary_author_office[["name"]], ", ",
-            primary_author_office[["city"]], ", ", 
-            primary_author_office[["state"]], ". "
-          )
-        },
-        {
-          # Default
-          paste0(
+      },
+      "NWFSC" = {
+        paste0(
+          "Prepared by [COMMITTEE]. [XX] p."
+        )
+      },
+      "SEFSC" = {
+        paste0(
+          "SEDAR, North Charleston SC. [XX] pp. ",
+          "available online at: http://sedarweb.org/"
+        )
+      },
+      "SWFSC" = {
+        paste0(
+          "Pacific Fishery Management Council, Portland, OR. Available from https://www.pcouncil.org/stock-assessments-and-fishery-evaluation-safe-documents/."
+        )
+      },
+      "PIFSC" = {
+        paste0(
+          "NOAA Tech. Memo. [TECH MEMO NUMBER]",
+          ", ", "[XX] p."
+        )
+      },
+      "NEFSC" = {
+        paste0(
+          primary_author_office[["name"]], ", ",
+          primary_author_office[["city"]], ", ",
+          primary_author_office[["state"]], ". "
+        )
+      },
+      {
+        # Default
+        paste0(
           ". National Marine Fisheries Service, ",
           "[CITY], [STATE]. "
-          )
-        }
-      )    
-      # Pull together parts of citation
-      citation <- paste0(
-        "{{< pagebreak >}} \n",
-        "\n",
-        "Please cite this publication as: \n",
-        "\n",
-        ifelse(primary_author_office[["office"]]=="SEFSC", "SEDAR.", author_list),
-        " ", year, ". ", 
-        ifelse(is.null(title), "[TITLE]", glue::glue("{title}")), ". ",
-        region_specific_part
-      )
-    }
+        )
+      }
+    )
+    # Pull together parts of citation
+    citation <- paste0(
+      "{{< pagebreak >}} \n",
+      "\n",
+      "Please cite this publication as: \n",
+      "\n",
+      ifelse(primary_author_office[["office"]] == "SEFSC", "SEDAR.", author_list),
+      " ", year, ". ",
+      ifelse(is.null(title), "[TITLE]", glue::glue("{title}")), ". ",
+      region_specific_part
+    )
+  }
 
   # Add citation as .qmd to add into template
   citation
--- R/create_template.R
+++ R/create_template.R
@@ -336,10 +336,11 @@ create_template <- function(
     # report name without type and region
     report_name_1 <- gsub(glue::glue("{type}_"), "", prev_report_name)
     # Extract species
-    species <- gsub("_",
-                    " ",
-                    gsub(glue::glue("{region}_"), "", report_name_1)
-                    )
+    species <- gsub(
+      "_",
+      " ",
+      gsub(glue::glue("{region}_"), "", report_name_1)
+    )
 
 
     new_report_name <- paste0(
@@ -402,7 +403,7 @@ create_template <- function(
     grepl("/report", file_dir),
     fs::path(file_dir),
     fs::path(file_dir, "report")
-    )
+  )
 
   # Supporting files folder
   supdir <- file.path(subdir, "support_files")
@@ -469,21 +470,21 @@ create_template <- function(
         prev_format <- stringr::str_extract(
           prev_skeleton[grep("format:", prev_skeleton) + 1],
           "[a-z]+"
-          )
+        )
         year <- as.numeric(stringr::str_extract(
           prev_skeleton[grep("title:", prev_skeleton)],
           "[0-9]+"
-          ))
+        ))
         # if it is previously html and the rerender species html then need to copy over html formatting
         if (tolower(prev_format) != "html" & tolower(format) == "html") {
-         if (!file.exists(file.path(file_dir, "support_files", "theme.scss"))) file.copy(system.file("resources", "formatting_files", "theme.scss", package = "asar"), supdir, overwrite = FALSE) |> suppressWarnings()
+          if (!file.exists(file.path(file_dir, "support_files", "theme.scss"))) file.copy(system.file("resources", "formatting_files", "theme.scss", package = "asar"), supdir, overwrite = FALSE) |> suppressWarnings()
         }
         if (tolower(prev_format != "pdf" & tolower(format) == "pdf")) {
           if (is.null(species)) {
             species <- tolower(stringr::str_extract(
               prev_skeleton[grep("species: ", prev_skeleton)],
               "(?<=')[^']+(?=')"
-              ))
+            ))
           }
           if (is.null(office)) {
             office <- stringr::str_extract(
@@ -556,49 +557,49 @@ create_template <- function(
       # Convert output file if TRUE
       # Make sure not asking to rerender
       # if (!rerender_skeleton) {
-        # Check if converted output already exists
-        if (convert_output) {
-          if (!file.exists(file.path(subdir, paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv", sep = "")))) {
-            print("__________Converting output file__________")
-            if (tolower(model) == "bam" & is.null(fleet_names)) {
-              # warning("Fleet names not defined.")
-              convert_output(
-                output_file = model_results,
-                outdir = resdir,
-                file_save = TRUE,
-                model = model,
-                savedir = subdir,
-                save_name = paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, sep = "")
-              )
-              # } else if (tolower(model) == "bam") {
-              #   convert_output(
-              #     output_file = model_results,
-              #     outdir = resdir,
-              #     file_save = TRUE,
-              #     model = model,
-              #     fleet_names = fleet_names,
-              #     savedir = subdir,
-              #     save_name = paste(sub(" ", "_", species), "_std_res_", year, sep = "")
-              #   )
-            } else {
-              convert_output(
-                output_file = model_results,
-                outdir = resdir,
-                file_save = TRUE,
-                model = model,
-                savedir = subdir,
-                save_name = paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, sep = "")
-              )
-            }
-            # Rename model results file and results file directory if the results are converted in this fxn
-            model_results <- paste0(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv")
-            resdir <- subdir
+      # Check if converted output already exists
+      if (convert_output) {
+        if (!file.exists(file.path(subdir, paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv", sep = "")))) {
+          print("__________Converting output file__________")
+          if (tolower(model) == "bam" & is.null(fleet_names)) {
+            # warning("Fleet names not defined.")
+            convert_output(
+              output_file = model_results,
+              outdir = resdir,
+              file_save = TRUE,
+              model = model,
+              savedir = subdir,
+              save_name = paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, sep = "")
+            )
+            # } else if (tolower(model) == "bam") {
+            #   convert_output(
+            #     output_file = model_results,
+            #     outdir = resdir,
+            #     file_save = TRUE,
+            #     model = model,
+            #     fleet_names = fleet_names,
+            #     savedir = subdir,
+            #     save_name = paste(sub(" ", "_", species), "_std_res_", year, sep = "")
+            #   )
           } else {
-            message("Output not converted: standard output already in path.")
-            model_results <- paste0(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv")
-            resdir <- subdir
+            convert_output(
+              output_file = model_results,
+              outdir = resdir,
+              file_save = TRUE,
+              model = model,
+              savedir = subdir,
+              save_name = paste(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, sep = "")
+            )
           }
-        } # close check for converted output already
+          # Rename model results file and results file directory if the results are converted in this fxn
+          model_results <- paste0(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv")
+          resdir <- subdir
+        } else {
+          message("Output not converted: standard output already in path.")
+          model_results <- paste0(stringr::str_replace_all(species, " ", "_"), "_std_res_", year, ".csv")
+          resdir <- subdir
+        }
+      } # close check for converted output already
       # } # close rerender if
 
       # print("_______Standardized output data________")
@@ -732,8 +733,8 @@ create_template <- function(
       # Pull authors and affiliations from national db
       # Check if rerender and if author is already added
       # TODO: add feature to allow removal of authors if there are ones that
-        # are repeated from the previous skeleton and those named (not just
-        # additions of new names)
+      # are repeated from the previous skeleton and those named (not just
+      # additions of new names)
       if (rerender_skeleton) {
         # Pull all author names from prev_skeleton
         author_prev <- grep(
@@ -1040,11 +1041,12 @@ create_template <- function(
               preamble[prev_results_line],
               "(?<=read\\.csv\\().*?(?=\\))",
               glue::glue("'{resdir}/{model_results}'")
-              )
+            )
             preamble <- append(
               preamble,
               prev_results,
-              after = prev_results_line)[-prev_results_line]
+              after = prev_results_line
+            )[-prev_results_line]
             if (!grepl(".csv", model_results)) warning("Model results are not in csv format - Will not work on render")
           } else {
             message("Preamble maintained - model results not updated.")
@@ -1088,8 +1090,8 @@ create_template <- function(
           prev_skeleton,
           "(?<=['`])[^']+\\.qmd(?=['`])"
         ) |>
-        unlist() |>
-        purrr::discard(~ .x == "")
+          unlist() |>
+          purrr::discard(~ .x == "")
         # add sections as list
         sections <- add_child(
           sections,
@@ -1180,7 +1182,7 @@ create_template <- function(
             )
             # Create sections object to add into template
             custom_sections <- gsub(".qmd", "", unlist(sec_list2))
-            custom_sections <- sub("^[0-9]+_","", custom_sections)
+            custom_sections <- sub("^[0-9]+_", "", custom_sections)
             custom_sections <- sub("^[0-9]+[a-z]_", "", custom_sections)
             sections <- add_child(
               sec_list2,
@@ -1213,7 +1215,7 @@ create_template <- function(
             # Create sections object to add into template
             # name of chunks
             custom_sections <- gsub(".qmd", "", unlist(sec_list2))
-            custom_sections <- sub("^[0-9]+_","", custom_sections)
+            custom_sections <- sub("^[0-9]+_", "", custom_sections)
             custom_sections <- sub("^[0-9]+[a-z]_", "", custom_sections)
             sections <- add_child(
               sec_list2,
