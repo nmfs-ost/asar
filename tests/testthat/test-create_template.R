d6a112a2a9eb23711eb3c967b9ae029b6625894e
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2025-05-12 11:23:56 -0400

    Revert "Glossary notation (#218)" (#238)
    This reverts commit 777c0118a22d3a8bf90974d7cba914f16a8757c8.

--- tests/testthat/test-create_template.R
+++ tests/testthat/test-create_template.R
@@ -41,7 +41,6 @@ test_that("create_template() creates correct files", {
     "10_notes.qmd",
     "11_appendix.qmd",
     "SAR_species_skeleton.qmd",
-    "report_glossary.tex",
     "asar_references.bib",
     "support_files"
   )
@@ -117,7 +116,6 @@ test_that("create_template() creates correct files", {
     "11_appendix.qmd",
     "SAR_Dover_sole_skeleton.qmd",
     "asar_references.bib",
-    "report_glossary.tex",
     "support_files"
   )
   # Define expected support files for Dover sole
