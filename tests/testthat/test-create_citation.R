717ff4a938b74cbcfd12f0e09b93eb466c928cb8
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-05-14 11:37:22 -0400

    update test create_citation

--- tests/testthat/test-create_citation.R
+++ tests/testthat/test-create_citation.R
@@ -9,6 +9,6 @@ test_that("citation generated properly for SWFSC", {
     title = "Check",
     year = 2024
   )
-  expected_output <- "{{< pagebreak >}} \n\nPlease cite this publication as: \n\nKuriyama, P. T., Allen Akselrud, C., Zwolinski, J. P., and Hill, K. T. 2024. Check. Pacific Fishery Management Council, Portland, OR. Available from https://www.pcouncil.org/stock-assessments-and-fishery-evaluation-safe-documents/."
+  expected_output <- "{{< pagebreak >}} \n\nPlease cite this publication as: \n\nKuriyama, P. T., Allen Akselrud, C., Zwolinski, J. P., and Hill, K. T. 2024. Check. Pacific Fishery Management Council, Portland, OR. Available from https://www.pcouncil.org/stock-assessments-and-fishery-evaluation-safe-documents/. \\pageref*{LastPage}{} pp."
   expect_equal(fxn_test, expected_output)
 })
