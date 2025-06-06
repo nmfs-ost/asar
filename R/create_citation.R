614333c11c66749a02741f2f851bd90d9ee2b18d
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-06-06 10:58:39 -0400

    Update R/create_citation.R
    Co-authored-by: Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>

--- R/create_citation.R
+++ R/create_citation.R
@@ -19,7 +19,7 @@ create_citation <- function(
     year = format(as.POSIXct(Sys.Date(), format = "%YYYY-%mm-%dd"), "%Y")) {
   # Check if author is input - improved from previous fxn so did not fail
     if (any(author == "")) {
-      cli::cli_alert_warning("Authorship is not defined.")
+      cli::cli_alert("Authorship is not defined.")
       # Define default citation - needs author editing
       citation <- paste0(
         "{{< pagebreak >}} \n",
