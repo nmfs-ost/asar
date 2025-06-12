cf6a762102ba335e743d49726426c5fe783ccd66
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-06-12 12:32:34 -0400

    updates to fix authorship issue in rerender

--- R/add_authors.R
+++ R/add_authors.R
@@ -17,19 +17,17 @@ add_authors <- function(
     author,
     rerender_skeleton = FALSE,
     prev_skeleton = NULL) {
-  message("pre-processing")
   # Set author into proper format - will get overwritten later if rerender = T
   author_names <- names(author)
   # Get authors into readable format for ordering
   authors <- data.frame(name = author_names, office = author, row.names = NULL)
-  message("author df ready")
   
   # Check if rerender and if author is already added
   # TODO: add feature to allow removal of authors if there are ones that
   # are repeated from the previous skeleton and those named (not just
   # additions of new names)
   if (rerender_skeleton) {
-    message("continuing process for rerender")
+    
     if (is.null(prev_skeleton)) {
       # attempt to find the skeleton file
       if (file.exists(file.path(getwd(), list.files(file_dir, pattern = "skeleton.qmd")))) {
@@ -38,24 +36,30 @@ add_authors <- function(
         cli::cli_abort("No skeleton quarto file found in the working directory.")
       }
     }
-    message("passed if skeleton found")
+    
     # Pull all author names from prev_skeleton
     author_prev <- grep(
       "\\- name:\\s*'",
       prev_skeleton,
       value = TRUE
     )
+    
     # Remove every second occurance of "-name"
-    author_prev <- author_prev[seq(1, length(author_prev), 2)]
+    if (length(author_prev) <= 2) {
+      author_prev <- author_prev[1]
+    } else {
+      author_prev <- author_prev[seq(1, length(author_prev), 2)]
+    }
+    
     # Remove everything but the name
     author_prev <- sub(
       ".*\\- name:\\s*'([^']+)'.*",
       "\\1",
       author_prev
     )
-    message("extracted previous author(s)")
+    
     setdiff(author_names, author_prev) -> author2
-    message("extracted new authors (if any)")
+    
     # return if null
     if (is.null(author2)) {
       return(NULL) # this line should stop the function here 
@@ -65,11 +69,10 @@ add_authors <- function(
       authors <- dplyr::filter(authors, name %in% author2)
     }
   }
-  message("ended processing new authors on rerender")
+  
   # Load in affiliation
   affil <- utils::read.csv(system.file("resources", "affiliation_info.csv", package = "asar", mustWork = TRUE))
   
-  message("start getting authors in yaml format")
   author_list <- list()
   if (any(authors$name != "1")) { # nrow(authors) > 0 |
     # print("inside author==1")
