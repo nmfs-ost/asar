fc8a7cbfd8b9d220f53d9e487212e6f91c8ec767
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-06-18 12:50:37 -0400

    Cleanup pkg create template (#269)
    * remove include_figures, include_tables, and complex as arguments from create_template
    
    * remove fleet_names and prev_year as arguments from create_template; remove resdir in create_template body
    
    * remove add_image argument from create_template and replace use with spp_image; update documentation
    
    * remove affiliation-related arguments from create_template; update documentation
    
    * Remove deprecated args include_affiliation, convert_output, and resdir from add_accessibility and add_alttext examples; remove alt_title and convert_output from create_template example; remove add_image from create_yaml
    
    * Add complex argument definition to create_title, and update documentation, since it was inherited from create_template where it was removed
    
    * Add rda_dir to create_figures/tables_doc as an argument; update documentation
    
    * remove add_author arg
    
    * move authorship out of create_template into its own function and change function set up so that any author can be added with their aff
    
    * adjust create_yaml so that author arg is no longer used - basically it was pointless
    
    * add explicit of author in create_citation within create_template
    
    * made arg defaults explicit in documentation and change code as needed
    
    * add option to fix user entry for format and add in argument default
    
    * remove switch code
    
    * add in new affiliation options
    
    * fix issue with citation after author change
    
    * add option in add_authors where office can be nothing
    
    * change useage of model_results throughout create_template - mostly done in previous PR so only changes were to the examples
    
    * remove DRAFT watermark from documents - replaced by disclaimer
    
    * improve arg descriptions with authorship stuff
    
    * adjust default template when no author input
    
    * update use of chunk_option as arg and other arguments in functions that rely on add_chunk
    
    * set default title to [TITLE]
    
    * various tweaks so package works together
    
    * order arguments by importance in create_template
    
    * add missing argument to add_authors and fix issue with not adding author on rerender
    
    * update and work on fixing bugs in create_template and add-author
    
    * add messages to add_authors to debug
    
    * updates to fix authorship issue in rerender
    
    * add change to statement if model_results are input
    
    * push hex to see in README
    
    * remove hex since it was just a test
    
    * fix issue where preamble chunk stays false when updating model_results
    
    * updates to fxns to make more informative error messages after fixing bugs
    
    * documentation updates:
    
    * Update R/create_template.R
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update tests/testthat/test-add_chunk.R
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update tests/testthat/_snaps/add_chunk.md
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update tests/testthat/_snaps/add_chunk.md
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update tests/testthat/_snaps/add_chunk.md
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update R/create_yaml.R
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update R/create_template.R
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * Update R/create_template.R
    
    Co-authored-by: Sophie Breitbart <sophie.breitbart@noaa.gov>
    
    * make updates per review by @sbreitbart-NOAA
    
    ---------
    
    Co-authored-by: Schiano-NOAA <samantha.schiano@noaa.gov>
    Co-authored-by: Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>

--- R/create_tables_doc.R
+++ R/create_tables_doc.R
@@ -14,9 +14,7 @@
 #' share the same caption. To specify a different repeated column(s), use
 #' asar::export_split_tbls with your preferred essential_columns value.
 #'
-#' @inheritParams create_template
-#' @param subdir subdirectory where the assessment report template is being stored
-#' @param include_all include all default tables for a stock assessment report
+#' @inheritParams create_figures_doc
 #'
 #' @return Create a quarto document as part of a stock assessment outline with
 #' pre-loaded R chunks that add stock assessment tables from the nmfs-ost/stockplotr
@@ -57,9 +55,14 @@ create_tables_doc <- function(subdir = getwd(),
       add_chunk(
         paste0("library(flextable)\nrda_dir <- '", rda_dir, "/rda_files'"),
         label = "set-rda-dir-tbls",
-        eval = "true",
-        add_option = TRUE,
-        chunk_op = "include: false"
+        # eval = "true",
+        # add_option = TRUE,
+        chunk_option = c(
+          "echo: false",
+          "warning: false",
+          "eval: true",
+          "include: false"
+        )
       ),
       "\n"
     )
@@ -122,8 +125,8 @@ eval_", tab_shortname, " <- TRUE\n
 # if the table rda does not exist, don't evaluate the next chunk
 } else {eval_",  tab_shortname, " <- FALSE}"
           ),
-          label = paste0("tab-", tab_shortname, "-setup"),
-          eval = "true"
+          label = paste0("tab-", tab_shortname, "-setup")
+          # eval = "true"
         ),
 "\n"
 )
@@ -134,9 +137,14 @@ eval_", tab_shortname, " <- TRUE\n
             add_chunk(
               paste0(tab_shortname, "_table"),
               label = paste0("tbl-", tab_shortname),
-              eval = paste0("!expr eval_", tab_shortname),
-              add_option = TRUE,
-              chunk_op = c(
+              # eval = paste0("!expr eval_", tab_shortname),
+              # add_option = TRUE,
+              chunk_option = c(
+                "echo: false",
+                "warnings: false",
+                glue::glue(
+                  "eval: !expr if(eval_{tab_shortname}) {tab_shortname}_alt_text"
+                ),
                 glue::glue(
                   "tbl-cap: !expr if(eval_", tab_shortname, ") ", tab_shortname, "_cap"
                 ),
@@ -159,9 +167,14 @@ eval_", tab_shortname, " <- TRUE\n
                 flextable::fit_to_width(max_width = 8)"
               ),
             label = paste0("tbl-", tab_shortname),
-            eval = paste0("!expr eval_", tab_shortname),
-            add_option = TRUE,
-            chunk_op = c(
+            # eval = paste0("!expr eval_", tab_shortname),
+            # add_option = TRUE,
+            chunk_option = c(
+              "echo: false",
+              "warnings: false",
+              glue::glue(
+                "eval: !expr if(eval_{tab_shortname}) {tab_shortname}_alt_text"
+              ),
               glue::glue(
                 "tbl-cap: !expr if(eval_", tab_shortname, ") ", tab_shortname, "_cap"
               ),
@@ -201,9 +214,16 @@ eval_", tab_shortname, " <- TRUE\n
              )
              ,
              label = paste0("tbl-", tab_shortname, "-labels"),
-             eval = paste0("!expr eval_", tab_shortname),
-             add_option = TRUE,
-             chunk_op = c(glue::glue("include: false"))
+             # eval = paste0("!expr eval_", tab_shortname),
+             # add_option = TRUE,
+             chunk_option = c(
+               "echo: false",
+               "warnings: false",
+               glue::glue(
+                 "eval: !expr if(eval_{tab_shortname}) {tab_shortname}_alt_text"
+               ),
+               glue::glue("include: false")
+             )
            ),
            "\n"
          )
@@ -224,7 +244,7 @@ eval_", tab_shortname, " <- TRUE\n
               label = paste0("tbl-", tab_shortname, i),
               eval = paste0("!expr eval_", tab_shortname),
               add_option = TRUE,
-              chunk_op = c(
+              chunk_option = c(
                 glue::glue(
                   "tbl-cap: !expr if(eval_", tab_shortname, ") paste0(", tab_shortname, "_cap, '(', ", tab_shortname, "_cap_split[[", i, "]], ')')"
                 ),
