676c121580358db99980e85f88e8291f3318fafe
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-06-25 09:16:37 -0400

    Update create_figures/tables_doc tests; update .R files to ensure starts at header 1 (one # vs 2 ##)

--- tests/testthat/test-create_figures_doc.R
+++ tests/testthat/test-create_figures_doc.R
@@ -1,4 +1,4 @@
-test_that("Creates expected start of figures doc", {
+test_that("Creates expected start of nearly empty figures doc", {
   # create tables doc
   create_figures_doc(
     subdir = getwd(),
@@ -14,6 +14,7 @@ test_that("Creates expected start of figures doc", {
 
   # expected figures doc head
   expected_head_figure_content <- "# Figures {#sec-figures}"
+
  # expected_head_figure_content <- "## Figures {#sec-figures} ```{r} #| label: 'set-rda-dir-figs'#| echo: false #| warning: false #| eval: true "
 
   # test expectation of start of figures doc
@@ -26,4 +27,45 @@ test_that("Creates expected start of figures doc", {
 
   # erase temporary testing files
   file.remove(fs::path(getwd(), "09_figures.qmd"))
+})
+
+test_that("Creates expected start of figures doc with figure", {
+
+  # convert sample dataset
+  dat <- convert_output(
+    output_file = file.path("fixtures", "ss3_models", "models", "Hake_2018",
+                            "Report.sso"),
+    model = "ss3"
+  )
+
+  stockplotr::plot_landings(dat,
+                            make_rda = TRUE,
+                            rda_dir = getwd())
+
+  # create tables doc
+  create_figures_doc(
+    subdir = getwd(),
+    rda_dir = getwd()
+  )
+
+  # read in tables doc
+  figure_content <- readLines("09_figures.qmd")
+  # extract first 7 lines
+  head_figure_content <- head(figure_content, 7)
+  # remove line numbers and collapse
+  fc_pasted <- paste(head_figure_content, collapse = "")
+
+  # expected figures doc head
+  expected_head_figure_content <- "# Figures {#sec-figures} ```{r} #| label: 'set-rda-dir-figs'#| echo: false #| warnings: false #| eval: true"
+
+  # test expectation of start of figures doc
+  testthat::expect_equal(
+    fc_pasted,
+    expected_head_figure_content
+  )
+
+  # erase temporary testing files
+  file.remove(fs::path(getwd(), "09_figures.qmd"))
+  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+  unlink(fs::path(getwd(), "rda_files"), recursive = T)
 })
