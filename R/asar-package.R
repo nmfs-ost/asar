449998791fc03bc52a8bb404ea617782e48eb604
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-06-26 12:45:07 -0400

    add undefined variables

--- R/asar-package.R
+++ R/asar-package.R
@@ -15,6 +15,7 @@ globvar <- c(
   "input", "first", "mi", "first_initial", "bib", "output_order", "keyword",
   "output", "sexes", "like", "era", "init", "parm_stdev", "Component",
   "row_rescale", "table_list", "read.csv", "All", "x", "Acronym", "Meaning",
-  "meaning_lower", "Definition", "n", "Label"
+  "meaning_lower", "Definition", "n", "Label", "figures_doc_header",
+  "X"
 )
 if (getRversion() >= "2.15.1") utils::globalVariables(globvar)
