4ac2b172c0d679da59716e6d7802c090402be00c
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-07-07 08:21:09 -0400

    Uncomment tests (#308)
    * Updating and uncommenting some tests after new asar release
    
    * update readme convert_output argument
    
    * Fix ID_tbl_width_class() and update test
    
    * uncomment and update create_template tests
    
    * update add_chunk snapshot
    
    * update convert_output tests; add new test that creates rda; add rda
    
    * update create_figures/tables_doc tests with rda instead of code that converts output
    
    * Update create_template test; comment test that's still failing
    
    * fix final create_template test
    
    ---------
    
    Co-authored-by: Schiano-NOAA <samantha.schiano@noaa.gov>

--- tests/testthat/test-convert_output.R
+++ tests/testthat/test-convert_output.R
@@ -10,7 +10,7 @@ test_that("convert_output works for SS3", {
   for (i in seq_along(all_models)) {
     # Ensure no errors occur while converting SS3 output
     expect_no_error(result <- convert_output(
-      output_file = file.path(all_models[i], "Report.sso"),
+      file = file.path(all_models[i], "Report.sso"),
       model = "ss3"
     ))
 
@@ -20,8 +20,24 @@ test_that("convert_output works for SS3", {
 
   # Test saving the output in a global environment
   output <- convert_output(
-    output_file = file.path(all_models[1], "Report.sso"),
+    file = file.path(all_models[1], "Report.sso"),
     model = "ss3"
   )
   expect_equal(dim(output)[2], 36)
+})
+
+
+test_that("convert_output saves model ss3 hake output file", {
+  conv_mod_dir <- fs::path("fixtures", "ss3_models_converted", "Hake_2018", "std_output.rda")
+
+  file.remove(fs::path(conv_mod_dir))
+
+  asar::convert_output(
+    file = fs::path("fixtures", "ss3_models", "models", "Hake_2018", "Report.sso"),
+    model = "ss3",
+    save_dir = fs::path("fixtures", "ss3_models_converted", "Hake_2018")
+  )
+
+  expect_true(list.files(fs::path("fixtures", "ss3_models_converted", "Hake_2018")) == "std_output.rda")
+
 })
--- tests/testthat/test-create_tables_doc.R
+++ tests/testthat/test-create_tables_doc.R
@@ -27,45 +27,39 @@ test_that("Creates expected start of nearly empty tables doc", {
   file.remove(fs::path(getwd(), "08_tables.qmd"))
 })
 
-# this won't pass until stockplotr is updated to output to 'figures' and 'tables'
-# folders rather than "rda_dir"
-# test_that("Creates expected start of tables doc with table", {
-#
-#   # convert sample dataset
-#   dat <- convert_output(
-#     output_file = file.path("fixtures", "ss3_models", "models", "Hake_2018",
-#                             "Report.sso"),
-#     model = "ss3"
-#   )
-#
-#   stockplotr::table_landings(dat,
-#                             make_rda = TRUE,
-#                             rda_dir = getwd())
-#
-#   # create tables doc
-#   create_tables_doc(
-#     subdir = getwd(),
-#     tables_dir = getwd()
-#   )
-#
-#   # read in tables doc
-#   table_content <- readLines("08_tables.qmd")
-#   # extract first 7 lines
-#   head_table_content <- head(table_content, 7)
-#   # remove line numbers and collapse
-#   fc_pasted <- paste(head_table_content, collapse = "")
-#
-#   # expected tables doc head
-#   expected_head_table_content <- "# Tables {#sec-tables} ```{r} #| label: 'set-rda-dir-tbls'#| echo: false #| warning: false #| eval: true "
-#
-#   # test expectation of start of tables doc
-#   testthat::expect_equal(
-#     fc_pasted,
-#     expected_head_table_content
-#   )
-#
-#   # erase temporary testing files
-#   file.remove(fs::path(getwd(), "08_tables.qmd"))
-#   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
-#   unlink(fs::path(getwd(), "rda_files"), recursive = T)
-# })
+test_that("Creates expected start of tables doc with table", {
+
+  # load sample dataset
+  load(file.path("fixtures", "ss3_models_converted", "Hake_2018",
+                 "std_output.rda"))
+
+  stockplotr::table_landings(out_new,
+                            make_rda = TRUE)
+
+  # create tables doc
+  create_tables_doc(
+    subdir = getwd(),
+    tables_dir = getwd()
+  )
+
+  # read in tables doc
+  table_content <- readLines("08_tables.qmd")
+  # extract first 7 lines
+  head_table_content <- head(table_content, 7)
+  # remove line numbers and collapse
+  fc_pasted <- paste(head_table_content, collapse = "")
+
+  # expected tables doc head
+  expected_head_table_content <- "# Tables {#sec-tables} ```{r} #| label: 'set-rda-dir-tbls'#| echo: false #| warning: false #| include: false"
+
+  # test expectation of start of tables doc
+  testthat::expect_equal(
+    fc_pasted,
+    expected_head_table_content
+  )
+
+  # erase temporary testing files
+  file.remove(fs::path(getwd(), "08_tables.qmd"))
+  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+  unlink(fs::path(getwd(), "tables"), recursive = T)
+})
